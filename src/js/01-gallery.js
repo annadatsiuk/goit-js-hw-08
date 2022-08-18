7305f1a3b2e5e94835fa4da7ef6a380abc34919e
Anna Datsiuk <anna.datsiuk94@gmail.com>
2022-08-18 18:45:01 +0300

    ready first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector(".gallery");
+
+const listItemsMarkup = createListItemsMarkup(galleryItems);
+list.innerHTML = listItemsMarkup;
+
+function createListItemsMarkup(items) {
+    return items
+        .map(
+            (item) =>
+                `
+            <a class="gallery__link" href="${item.original}">
+                <img
+                    class="gallery__image"
+                    src="${item.preview}"
+                    data-source="${item.original}"
+                    alt="${item.description}"
+                />
+            </a>`
+        )
+        .join("");
+}
+new SimpleLightbox('.gallery a', {captionDelay: 250, captionsData: 'alt'});
