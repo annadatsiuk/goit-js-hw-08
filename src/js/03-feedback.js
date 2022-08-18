b28ee97a04e1e5e90a2edbe5803c75889fc4d9f9
Anna Datsiuk <anna.datsiuk94@gmail.com>
2022-08-18 20:15:47 +0300

    final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const feedbackFormRef = document.querySelector('.feedback-form');
+
+feedbackFormRef.addEventListener('input', throttle(onFieldsFormInput, 500));
+feedbackFormRef.addEventListener('submit', onSubmitForm);
+
+populateFieldForm();
+
+function onFieldsFormInput(e) {
+  let formData = localStorage.getItem(STORAGE_KEY);
+
+  formData = formData ? JSON.parse(formData) : {};
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateFieldForm() {
+  let formData = localStorage.getItem(STORAGE_KEY);
+
+  if (formData) {
+    formData = JSON.parse(formData);
+    Object.entries(formData).forEach(
+      ([name, value]) => (feedbackFormRef.elements[name].value = value),
+    );
+  }
+}
